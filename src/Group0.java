58e676b8d261f319388e91aa9d042c439f8289aa
Nik Bailey <bail0301@morris.umn.edu>
2022-10-12 09:52:48 -0500

    Make all of the compare methods, and start a "main" class
    Not all of the compares work yet, but we started. Also we haven't ran main yet so we don't know if the set up is very good, but we copied everything so hopefully its ok.

--- src/Group0.java
+++ src/Group0.java
@@ -20,15 +20,15 @@ public class Group0 {
 
 		String inputFileName = args[0];
 		String outFileName = args[1];
-
-		runTests();
+		
+		//runTests();
 
 		String[] data = readData(inputFileName); // read data as strings
-
+		
 		String[] toSort = data.clone(); // clone the data
 
 		sort(toSort); // call the sorting method once for JVM warmup
-
+		
 		toSort = data.clone(); // clone again
 
 		Thread.sleep(10); // to let other things finish before timing; adds stability of runs
@@ -44,6 +44,7 @@ public class Group0 {
 		writeOutResult(toSort, outFileName); // write out the results
 
 	}
+
 
 	private static String[] readData(String inputFileName) throws FileNotFoundException {
 		ArrayList<String> input = new ArrayList<>();
@@ -72,41 +73,42 @@ public class Group0 {
 
 		@Override
 		public int compare(String o1, String o2) {
-			if (o1.contains("/")) {
-				if (o2.contains("/")) {
+			if(o1.contains("/")){
+				if(o2.contains("/")){
 					return compareFractions(o1, o2);
-				} else {
+				}else{
 					return compareFractionAndDecimal(o1, o2);
 				}
 
-			} else {
-				if (o2.contains("/")) {
+			}else{
+				if(o2.contains("/")){
 					return -compareFractionAndDecimal(o2, o1);
-				} else {
-					return compareDecimals(o1, o2);
+				}else{
+					return compareDecimals(o1,o2);
 				}
 			}
 
 		}
 
-		private int compareFractionAndDecimal(String fraction, String decimal) {
+		
+		private int compareFractionAndDecimal(String fraction,String decimal) {
 			String[] saFrac = fraction.split("/");
 			BigInteger numerator1 = new BigInteger(saFrac[0]);
 			BigInteger denominator1 = new BigInteger(saFrac[1]);
-
-			// find the length of the decimal's fractional part
+			
+			//find the length of the decimal's fractional part
 			String[] saDec = decimal.split("\\."); // need \\ because . is a special symbol in regex
-
+			
 			BigInteger numerator2 = null;
-			BigInteger denominator2 = null;
-
+			BigInteger denominator2 = null; 				
+			
 			if (saDec.length == 1) { // an integer, positive or negative
 				numerator2 = new BigInteger(saDec[0]);
 				denominator2 = new BigInteger("1");
 			} else {
 				// find the length of the decimal part
 				int n = saDec[1].length();
-				denominator2 = new BigInteger("1");
+				denominator2 = new BigInteger("1"); 
 				// raising 10 to the power n
 				for (int i = 0; i < n; ++i) {
 					denominator2 = denominator2.multiply(new BigInteger("10"));
@@ -114,21 +116,20 @@ public class Group0 {
 				numerator2 = new BigInteger(saDec[1]);
 				// adding the integer part
 				BigInteger intPart = new BigInteger(saDec[0]);
-				if (saDec[0].charAt(0) == '-') { // the number is negative
+				if (saDec[0].charAt(0) == '-') { // the number is negative 
 					numerator2 = (intPart.multiply(denominator2)).subtract(numerator2);
 				} else {
 					numerator2 = numerator2.add(intPart.multiply(denominator2));
 				}
 			}
-
+			
 			BigInteger crossMult1 = numerator1.multiply(denominator2);
 			BigInteger crossMult2 = numerator2.multiply(denominator1);
-
+			
 			int res = crossMult1.compareTo(crossMult2);
-
-			if (res != 0)
-				return res;
-
+			
+			if (res != 0) return res;
+			
 			if (numerator1.compareTo(new BigInteger("0")) >= 0) {
 				return 1; // for positive, the decimal is smaller
 			} else {
@@ -139,33 +140,32 @@ public class Group0 {
 		private int compareFractions(String fraction1, String fraction2) {
 			// compare fraction by multiplication as big integers,
 			// to make sure we are not losing precision
-
+			
 			String[] sa1 = fraction1.split("/");
 			BigInteger numerator1 = new BigInteger(sa1[0]);
 			BigInteger denominator1 = new BigInteger(sa1[1]);
-
+			
 			String[] sa2 = fraction2.split("/");
 			BigInteger numerator2 = new BigInteger(sa2[0]);
 			BigInteger denominator2 = new BigInteger(sa2[1]);
-
+			
 			BigInteger crossMult1 = numerator1.multiply(denominator2);
 			BigInteger crossMult2 = numerator2.multiply(denominator1);
-
-			int res = crossMult1.compareTo(crossMult2);
-
-			if (res != 0)
-				return res;
-
-			return numerator1.compareTo(numerator2); // note: the numerator may be negative, that would reverse the
-														// ordering for negatives
+			
+			int res = crossMult1.compareTo(crossMult2);	
+			
+			if (res != 0) return res;
+			
+			return numerator1.compareTo(numerator2); // note: the numerator may be negative, that would reverse the ordering for negatives
 		}
+
 
 		private int compareDecimals(String o1, String o2) {
 			return (new BigDecimal(o1)).compareTo(new BigDecimal(o2));
 		}
 
 	}
-
+	
 	private static void writeOutResult(String[] sorted, String outputFilename) throws FileNotFoundException {
 		PrintWriter out = new PrintWriter(outputFilename);
 		for (String s : sorted) {
@@ -173,31 +173,29 @@ public class Group0 {
 		}
 		out.close();
 	}
-
+	
 	private static void runTests() {
 		SortingCompetitionComparator comp = new SortingCompetitionComparator();
-
+		
 		// Two fractions, positive and negative
-		System.out.println("-1/2 and 1/4:" + comp.compareFractions("-1/2", "1/4"));
-		System.out.println("1/2 and 1/3:" + comp.compareFractions("1/2", "1/3"));
-		System.out.println("1/2 and 2/4:" + comp.compareFractions("1/2", "2/4"));
-		System.out.println("-1/2 and -2/4:" + comp.compareFractions("-1/2", "-2/4"));
-
+		System.out.println("-1/2 and 1/4:" + comp.compareFractions("-1/2","1/4"));
+		System.out.println("1/2 and 1/3:" + comp.compareFractions("1/2","1/3"));
+		System.out.println("1/2 and 2/4:" + comp.compareFractions("1/2","2/4"));
+		System.out.println("-1/2 and -2/4:" + comp.compareFractions("-1/2","-2/4"));
+		
 		// Fraction and a decimal
-		System.out.println("1/4 and 0.5:" + comp.compareFractionAndDecimal("1/4", "0.5"));
-		System.out.println("2/4 and 1.5:" + comp.compareFractionAndDecimal("2/4", "1.5"));
-		System.out.println("-2/4 and -1.5:" + comp.compareFractionAndDecimal("-2/4", "-1.5"));
-		System.out.println("-2/4 and 0:" + comp.compareFractionAndDecimal("-2/4", "0"));
-		System.out.println("1/2 and -0.5:" + comp.compareFractionAndDecimal("1/2", "-0.5"));
-		System.out.println("1/2 and 0.5:" + comp.compareFractionAndDecimal("1/2", "0.5"));
-		System.out.println("-1/2 and -0.5:" + comp.compareFractionAndDecimal("-1/2", "-0.5"));
-		System.out.println("1/3 and -0.5:" + comp.compareFractionAndDecimal("1/3", "-0.5"));
-
-		System.out.println("-4.9999999999999999999999999999999999 and -5:"
-				+ comp.compareDecimals("-4.9999999999999999999999999999999999", "-5"));
-		System.out.println("4.9999999999999999999999999999999999 and 5:"
-				+ comp.compareDecimals("4.9999999999999999999999999999999999", "5"));
-
+		System.out.println("1/4 and 0.5:" + comp.compareFractionAndDecimal("1/4","0.5"));
+		System.out.println("2/4 and 1.5:" + comp.compareFractionAndDecimal("2/4","1.5"));
+		System.out.println("-2/4 and -1.5:" + comp.compareFractionAndDecimal("-2/4","-1.5"));
+		System.out.println("-2/4 and 0:" + comp.compareFractionAndDecimal("-2/4","0"));
+		System.out.println("1/2 and -0.5:" + comp.compareFractionAndDecimal("1/2","-0.5"));
+		System.out.println("1/2 and 0.5:" + comp.compareFractionAndDecimal("1/2","0.5"));
+		System.out.println("-1/2 and -0.5:" + comp.compareFractionAndDecimal("-1/2","-0.5"));
+		System.out.println("1/3 and -0.5:" + comp.compareFractionAndDecimal("1/3","-0.5"));
+		
+		System.out.println("-4.9999999999999999999999999999999999 and -5:" + comp.compareDecimals("-4.9999999999999999999999999999999999", "-5"));
+		System.out.println("4.9999999999999999999999999999999999 and 5:" + comp.compareDecimals("4.9999999999999999999999999999999999", "5"));
+		
 	}
-
+	
 }
--- src/funny.java
+++ src/funny.java
@@ -1,66 +1,158 @@
-public class funny {
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class funny  implements Comparator<String> {
     public static void main(String[] args) {
+
+        funny thing = new funny();
         
-        String potato = "0.11131111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111";
-        String salmon = "00.11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111";
+        String potato = "0.33333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333";
+        String salmon = "-0.21111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111";
 
-        String fraction = "1/1112";
-
-
-        char[] potatobutBetter = potato.toCharArray();
-        char[] salmonbutBetter = salmon.toCharArray();
-
-        if(isNegative(potatobutBetter) && !isNegative(salmonbutBetter)) {
-            System.out.println("Salmon better because it aint negative");
-            return;
-        }
-
-        if(!isNegative(potatobutBetter) && isNegative(salmonbutBetter)) {
-            System.out.println("Potato better because it aint negative");
-            return;
-        }
-
-
+        String fracOne = "1/3";
+        String fracTwo = "-1/2";
 
         
-        comparisonThingy(potatobutBetter, salmonbutBetter);
 
 
+        System.out.println("Result of Fraction compare: " + thing.compareFractions(fracTwo, fracOne));
+        System.out.println("Result of Decimal compare: " + thing.compareDecimals(potato, salmon));
+        System.out.println("Result of Decimal/Fraction compare: " + thing.compareFractionAndDecimal(potato, fracOne));
+        funny.runTests();
+
+    }
+
+    
+
+    @Override
+		public int compare(String o1, String o2) {
+			if(o1.contains("/")){
+				if(o2.contains("/")){
+					return compareFractions(o1, o2);
+				}else{
+					return compareFractionAndDecimal(o1, o2);
+				}
+
+			}else{
+				if(o2.contains("/")){
+					return -compareFractionAndDecimal(o2, o1);
+				}else{
+					return compareDecimals(o1,o2);
+				}
+			}
+
+		}
 
 
-        //System.out.println("If this is it, they're equal");
+    public int compareDecimals(String decimalOne, String decimalTwo) {
+        int isNegative = 1;
+        char[] decOne = decimalOne.toCharArray();
+        char[] decTwo = decimalTwo.toCharArray();
+
+        if(imposterCheck(decimalOne, decimalTwo) != 0 && imposterCheck(decimalOne, decimalTwo) != 2) return imposterCheck(decimalOne, decimalTwo);
+        if(imposterCheck(decimalOne, decimalTwo) == 2) isNegative = -1; 
+        int len = Math.min(decOne.length, decTwo.length);
+        for (int i = 0; i < len; i++) {
+                if(decOne[i] < decTwo[i]) {
+                    return -1*isNegative;
+                }
+                if(decOne[i] > decTwo[i]) {
+                    return 1*isNegative;
+                }
+        }
+
+        if(decOne.length > decTwo.length) return 1*isNegative;
+        else return -1*isNegative; // Equal and less than return the same thing, because we're funny
     }
 
 
-    public static boolean isNegative(char[] number){
-        if(number[0] == '-') return true;
+    public int compareFractions(String fractionOne, String fractionTwo) {
+        int isNegative = 1;
+        if(imposterCheck(fractionOne, fractionTwo) != 0 && imposterCheck(fractionOne, fractionTwo) != 2) return imposterCheck(fractionOne, fractionTwo);
+        if(imposterCheck(fractionOne, fractionTwo) == 2) isNegative = -1;
+
+        int[] fracOne = fracToInt(fractionOne);
+        int[] fracTwo = fracToInt(fractionTwo);
+
+            if(fracOne[0]*fracTwo[1] > fracTwo[0]*fracOne[1]){
+                return 1 * isNegative;
+            } else {
+                return -1 * isNegative; // If they're the same it doesn't matter plus if the first one isn't true then its the opposite
+            }
+    }
+
+    public int compareFractionAndDecimal(String Decimal, String Fraction) {
+
+        int[] fracityFrac = fracToInt(Fraction);
+        String deciFrac = Double.toString((double)fracityFrac[0]/(double)fracityFrac[1]);
+
+        return compareDecimals(Decimal, deciFrac);
+    }
+
+
+
+
+    // Helper function section
+
+    public boolean isNegative(String decimalOne) {
+        if(decimalOne.contains("-")) return true;
         return false;
     }
 
-    public static void comparisonThingy(char[] number, char[] anotherNumber){
-        int len = length(number, anotherNumber);
-        
-        
-        for (int i = 0; i < len; i++) {
-                if(number[i] < anotherNumber[i]){
-                    System.out.println("Salmon better");
-                    return;
-                }
-                if(number[i] > anotherNumber[i]){
-                    System.out.println("Potato better");
-                    return;
-                }
+    public int imposterCheck(String one, String two){
+        if(isNegative(one) && !isNegative(two)) {
+            return -1;
         }
-        System.out.println();
 
-        if(number.length - len > 0) System.out.println("Potato is better because it is longer");
-        else System.out.println("Salmon is better because it is longer");
+        if(!isNegative(one) && isNegative(two)) {
+            return 1;
+        } 
+        if(isNegative(one) && isNegative(two)) {
+            return 2;
+        }
+        return 0;
     }
 
-    public static int length(char[] number, char[] anotherNumber) {
-        if(number.length > anotherNumber.length) return number.length;
-        else return anotherNumber.length;
+    public int[] fracToInt(String fraction) {
+        int[] arr = new int[2];
+        int num = 0;
+        int denom = 0;
+        for(int i = 0; i < fraction.length(); i++) {
+            if(fraction.charAt(i) == '/') {
+                num = Integer.valueOf(fraction.substring(0, i));
+                denom = Integer.valueOf(fraction.substring(i + 1));
+            }
+        }
+
+        arr[0] = num;
+        arr[1] = denom;
+        return arr;
     }
+
+
+    private static void runTests() {
+		funny comp = new funny();
+		
+		// Two fractions, positive and negative
+		System.out.println("-1/2 and 1/4:" + comp.compareFractions("-1/2","1/4"));
+		System.out.println("1/2 and 1/3:" + comp.compareFractions("1/2","1/3"));
+		System.out.println("1/2 and 2/4:" + comp.compareFractions("1/2","2/4"));
+		System.out.println("-1/2 and -2/4:" + comp.compareFractions("-1/2","-2/4"));
+		
+		// Fraction and a decimal
+		System.out.println("1/4 and 0.5:" + comp.compareFractionAndDecimal("1/4","0.5"));
+		System.out.println("2/4 and 1.5:" + comp.compareFractionAndDecimal("2/4","1.5"));
+		System.out.println("-2/4 and -1.5:" + comp.compareFractionAndDecimal("-2/4","-1.5"));
+		System.out.println("-2/4 and 0:" + comp.compareFractionAndDecimal("-2/4","0"));
+		System.out.println("1/2 and -0.5:" + comp.compareFractionAndDecimal("1/2","-0.5"));
+		System.out.println("1/2 and 0.5:" + comp.compareFractionAndDecimal("1/2","0.5"));
+		System.out.println("-1/2 and -0.5:" + comp.compareFractionAndDecimal("-1/2","-0.5"));
+		System.out.println("1/3 and -0.5:" + comp.compareFractionAndDecimal("1/3","-0.5"));
+		
+		System.out.println("-4.9999999999999999999999999999999999 and -5:" + comp.compareDecimals("-4.9999999999999999999999999999999999", "-5"));
+		System.out.println("4.9999999999999999999999999999999999 and 5:" + comp.compareDecimals("4.9999999999999999999999999999999999", "5"));
+		
+	}
 
 
 
