d5b0e6390dda6aaf3e91cb1cf6daf25b114a3d98
Nik Bailey <bail0301@morris.umn.edu>
2022-10-13 10:17:04 -0500

    Add comments to the code

--- src/funny.java
+++ src/funny.java
@@ -23,7 +23,7 @@ public class funny  implements Comparator<String> {
     }
 
     
-
+// Stole this from group zero
     @Override
 		public int compare(String o1, String o2) {
 			if(o1.contains("/")){
@@ -45,13 +45,19 @@ public class funny  implements Comparator<String> {
 
 
     public int compareDecimals(String decimalOne, String decimalTwo) {
+        // We set this up, because if both of the inputs are negative technically the "bigger" value is smaller
         int isNegative = 1;
         char[] decOne = decimalOne.toCharArray();
         char[] decTwo = decimalTwo.toCharArray();
 
+        // imposterCheck is a helper function listed below (in helper function section)
         if(imposterCheck(decimalOne, decimalTwo) != 0 && imposterCheck(decimalOne, decimalTwo) != 2) return imposterCheck(decimalOne, decimalTwo);
         if(imposterCheck(decimalOne, decimalTwo) == 2) isNegative = -1; 
+        // Gets the minimum length so the index doesn't go out of bounds for either decimal
         int len = Math.min(decOne.length, decTwo.length);
+        // This cycles through checking each digit against each other
+        // Technically, starting on the left, each digit is more "valuable," so we use this as a shortcut
+        // However, this isn't great if they're equal
         for (int i = 0; i < len; i++) {
                 if(decOne[i] < decTwo[i]) {
                     return -1*isNegative;
@@ -61,52 +67,73 @@ public class funny  implements Comparator<String> {
                 }
         }
 
+        // If they are both equal for every digit in the range len, we double check that one isn't longer.
+        // However, this doesn't work well when we convert fractions, because the type double doesn't hold enough digits in some cases
+
+        // If the length of the first is less than the second, we say the second is longer.
+        // For the second case, we assume that in the case of two decimals the order doesn't matter,
+        // but we also assume the first one will be a converted fraction, so we have the first one defaulted as
+        // "bigger" for the case of equality.
         if(decOne.length < decTwo.length) return -1*isNegative;
-        else return 1*isNegative; // Equal and less than return the same thing, because we're funny
+        else return 1*isNegative; 
     }
 
 
     public int compareFractions(String fractionOne, String fractionTwo) {
+        // We set this up, because if both of the inputs are negative technically the "bigger" value is smaller
         int isNegative = 1;
+        // Imposter check is listed below
         if(imposterCheck(fractionOne, fractionTwo) != 0 && imposterCheck(fractionOne, fractionTwo) != 2) return imposterCheck(fractionOne, fractionTwo);
         if(imposterCheck(fractionOne, fractionTwo) == 2) isNegative = -1;
 
+        // fracToInt is listed below
         int[] fracOne = fracToInt(fractionOne);
         int[] fracTwo = fracToInt(fractionTwo);
 
+        // First numerator * second denominator and second numerator * first denominator should
+        // represent the fractions with equal denominators, and we do our comparisons with that.
         if(fracOne[0]*fracTwo[1] > fracTwo[0]*fracOne[1]) {
             return 1 * isNegative;
         } 
         if(fracOne[0]*fracTwo[1] < fracTwo[0]*fracOne[1]) {
             return -1 * isNegative;
         }
+        // If they happen to be equal, check which one has the larger denominator and return that as bigger
         if(fracOne[1] > fracTwo[1]) {
             return 1 * isNegative;
         }
         else {
-            return -1 * isNegative; // If they're the same it doesn't matter plus if the first one isn't true then its the opposite
+            return -1 * isNegative;
         }
     }
 
     public int compareFractionAndDecimal(String Fraction, String Decimal) {
-
+        // We set this up, because if both of the inputs are negative technically the "bigger" value is smaller
         double isNegative = 1;
+        // fracToInt is listed below
         int[] fracityFrac = fracToInt(Fraction);
-        if(imposterCheck(Fraction, Decimal) != 0 && imposterCheck(Fraction, Decimal) != 2) return imposterCheck(Fraction, Decimal);
-        if(imposterCheck(Fraction, Decimal) == 2) isNegative = -1; 
+        // isNegative is listed below
+        // This is still helpful here to keep the negative value, since fracToInt gets rid of the negative
+        if(isNegative(Fraction)) isNegative = -1;
+        // Convert the fraction into a decimal
         String deciFrac = Double.toString(((double)fracityFrac[0]/(double)fracityFrac[1])*isNegative);
 
+        // Then we use out compareDecimals method with the fraction in the first input spot.
+        // We specify why we do this in the compareDecimals method.
         return compareDecimals(deciFrac, Decimal);
     }
     
 
     // Helper function section
 
+    // Simple helper for return true or false if the first position in the string is a - sign
     public boolean isNegative(String num) {
         if(num.contains("-")) return true;
         return false;
     }
 
+    // ImposterCheck checks for if one, both or neither are negative. It will return 1 if the second is negative, -1 if the first is negative, 0 if both are positive,
+    // 2 if both are negative
     public int imposterCheck(String one, String two){
         if(isNegative(one) && !isNegative(two)) {
             return -1;
@@ -121,6 +148,11 @@ public class funny  implements Comparator<String> {
         return 0;
     }
 
+    // Converts a fractions into an int array,
+    // arr[0] contains the numerator and
+    // arr[1] contains the denominator
+    // This method doesn't keep the negative sign, if there is any
+    // so we will have to compensate for that in other methods
     public static int[] fracToInt(String fraction) {
         int[] arr = new int[2];
         int num = 0;
